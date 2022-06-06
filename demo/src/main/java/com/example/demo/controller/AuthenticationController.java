e0114aa62662896d16f072eeac85b721ecd1bd8c
Timo Ji <timo.ji@student.htw-berlin.de>
2022-06-06 18:39:49 +0200

    saveUserStateAndCode() and getCodeByUserState() implemented

--- demo/src/main/java/com/example/demo/controller/AuthenticationController.java
+++ demo/src/main/java/com/example/demo/controller/AuthenticationController.java
@@ -1,18 +1,33 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
 
 @RestController
 public class AuthenticationController {
 
-    @GetMapping("/authenticationHello")
-    String helloAuth() {
-        return "Hello AuthenticationController!";
+    HashMap<String, String> userStateAndCode = new HashMap<>();
+
+    @GetMapping()
+    String saveUserStateAndCode(@RequestParam(required = false, name = "state") String userState,@RequestParam(required = false) String code) {
+        if(code != null || userState != null){
+            userStateAndCode.put(userState, code);
+            String valueCode = userStateAndCode.get(userState);
+            return valueCode;
+    //        return "user state: " + userState + " - code: " + code;
+        }
+        return null;
     }
 
-    @GetMapping("/authenticationBye")
-    String byeAuth() {
-        return "Bye AuthenticationController!";
+    @GetMapping("/{userState}")
+    String getCodeByUserState(@PathVariable String userState) {
+        if(userState != null){
+            return userStateAndCode.get(userState);
+        }
+        return null;
     }
 }
