458d8ae4f1ce21a58167d61076c56236cc52a808
LuckyLuke16 <83580682+LuckyLuke16@users.noreply.github.com>
2022-06-07 16:31:03 +0200

    - feedback for unity-end added

--- demo/src/main/java/com/example/demo/controller/AuthenticationController.java
+++ demo/src/main/java/com/example/demo/controller/AuthenticationController.java
@@ -5,27 +5,30 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.awt.*;
+import java.net.http.HttpResponse;
 import java.util.HashMap;
-
 @RestController
 public class AuthenticationController {
 
     HashMap<String, String> userStateAndCode = new HashMap<>();
 
-    @GetMapping()
-    String saveUserStateAndCode(@RequestParam(required = false, name = "state") String userState,@RequestParam(required = false) String code) {
+    @GetMapping("/")
+    String saveUserStateAndCode(@RequestParam(required = false, name = "state") String userState,@RequestParam String code) {
         if(code != null || userState != null){
             userStateAndCode.put(userState, code);
             String valueCode = userStateAndCode.get(userState);
-            return valueCode;
+            return "Your code is automatically used to authenticate to Zoom: " + valueCode + "\n You can close this tab now.";
     //        return "user state: " + userState + " - code: " + code;
         }
         return null;
     }
 
-    @GetMapping("/{userState}")
+    @GetMapping("/code/{userState}")
     String getCodeByUserState(@PathVariable String userState) {
         if(userState != null){
+            if(userStateAndCode.get(userState)==null)
+                return "error no code found for given state";
             return userStateAndCode.get(userState);
         }
         return null;
